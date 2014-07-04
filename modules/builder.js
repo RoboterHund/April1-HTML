419379f0593ce3281fd604d6ecc3d315d78082cc
Alexander Diaz Chub <roboterhund@gmail.com>
2014-07-04 19:04:52 +0200

    added builder, types switched to april1 0.3.2

--- modules/builder.js
+++ modules/builder.js
@@ -1,8 +1,99 @@
-//
+// tag tree template builder
+// creates template with XML-like structure
 'use strict';
 
 var A = require ('april1');
 
-module.exports = {
+var types = require ('./types');
 
+var BUILD_ATTRS = 'attrs';
+
+function TagBuildState (prev) {
+	this.prev = prev;
+	this.open = null;
+	this.content = null;
+}
+
+function tag (builder, node) {
+	var state =
+		builder.buildStackTop =
+			new TagBuildState (builder.buildStackTop);
+
+	var tag = node [1];
+	state.open = [types.OPEN_TAG_PART, '<', tag];
+	state.content = [types.CONTENT_TAG_PART];
+
+	builder.setState (BUILD_ATTRS);
+	builder.build (node, 2);
+
+	if (state.content.length === 0) {
+		state.open.push ('/>');
+
+	} else {
+		state.open.push ('>');
+		var close = [types.CLOSE_TAG_PART, '</', tag, '>'];
+	}
+
+	builder.setState (A.common.BUILD);
+	builder.build (state.open);
+	builder.build (state.content);
+	if (close) {
+		builder.build (close);
+	}
+
+	builder.buildStackTop = builder.buildStackTop.prev;
+}
+
+function attr (builder, node) {
+	builder.buildStackTop.open.push (
+		' ', node [1], '="', node [2], '"');
+}
+
+function content (builder, node) {
+	builder.buildStackTop.content.push (node);
+}
+
+function tagBuilderStates () {
+	var states = {};
+	var dispatch;
+
+	// build attributes state
+	dispatch = {};
+	dispatch [types.ATTR] = attr;
+	dispatch [undefined] = content;
+	states [BUILD_ATTRS] = dispatch;
+
+	// build state
+	dispatch = A.common.builderDispatcher ();
+	dispatch [types.TAG] = tag;
+	states [A.common.BUILD] = dispatch;
+
+	return states;
+}
+var states = tagBuilderStates ();
+
+function tagBuilderParams () {
+	params = A.common.builderParams ();
+	params.states = states;
+}
+var params = params ();
+
+var template = A.common.template.buildTemplate.bind ({params: params});
+
+module.exports = {
+	BUILD_ATTRS: BUILD_ATTRS,
+
+	TagBuildState: TagBuildState,
+
+	tag: tag,
+	attr: attr,
+	content: content,
+
+	tagBuilderStates: tagBuilderStates,
+	tagBuilderParams: tagBuilderParams,
+
+	states: states,
+	params: params,
+
+	template: template
 };
--- modules/types.js
+++ modules/types.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-
+	ATTR: 'attr',
+	TAG: 'tag',
+	OPEN_TAG_PART: 't1',
+	CONTENT_TAG_PART: 't2',
+	CLOSE_TAG_PART: 't3'
 };
